f010376ac2a314f9bf748f630f7703a0bbbc36d4
Maasa <maasa.dev@gmail.com>
2020-10-15 14:30:40 -0700

    Declare and define saveIssue function

--- main.js
+++ main.js
@@ -1,17 +1,51 @@
-function fetchIssues() {
-    const issues = JSON.parse(localStorage.getItem('issues'));
-    const issuesList = document.getElementById('issuesList');
+document.getElementById("issueInputForm").addEventListener('submit', saveIssue);
 
-    issuesList.innerHTML = "";
+function saveIssue(e){
+    const issueDesc = document.getElementById("issueDescInput").value;
+    const issuePriority = document.getElementById("priorityLevelInput").value;
+    const issueAssignedTo = document.getElementById("issueAssignedTo").value;
+    const issueId = chance.guid();
+    const issueStatus = "Open";
 
-    for (let i = 0; i < issues.length; i++) {
-        const id = issues[i].id;
-        const desc = issues[i].description;
-        const severity = issues[i].severity;
-        const assignedTo = issues[i].assignedTo;
-        const status = issues[i].status;
+    const bug = {
+        id: issueId,
+        desc: issueDesc,
+        priority: issuePriority,
+        assignedTo: issueAssignedTo,
+        status: issueStatus
+    }
 
-        issuesList.innerHTML += '<div class="well">' + 
+    if (localStorage.getItem("issues") === null) {
+        const issues = [];
+        issues.push(issue);
+        localStorage.setItem("issues", JSON.stringify(issues));
+    } else {
+        const issues = JSON.parse(localStorage.getItem("issues"));
+        issues.push(issue);
+        localStorage.setItem("issues", JSON.stringify(issues));
+    }
+
+    document.getElementById("issueInpuForm").reset();
+
+    fetchIssues();
+
+    e.preventDefault();
+}
+
+function fetchbugs() {
+    const bugs = JSON.parse(localStorage.getItem('bugs'));
+    const bugsList = document.getElementById('bugsList');
+
+    bugsList.innerHTML = "";
+
+    for (let i = 0; i < bugs.length; i++) {
+        const id = bugs[i].id;
+        const desc = bugs[i].description;
+        const severity = bugs[i].severity;
+        const assignedTo = bugs[i].assignedTo;
+        const status = bugs[i].status;
+
+        bugsList.innerHTML += '<div class="well">' + 
                                 '<h6>Issue ID: ' + id + '<h6>' + 
                                 '<p><span class="label label-info">' + status + '</span></p>' + 
                                 '<h3>' + desc + '</h3>' + 
